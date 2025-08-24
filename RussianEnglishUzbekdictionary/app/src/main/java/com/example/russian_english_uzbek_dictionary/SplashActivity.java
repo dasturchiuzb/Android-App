2dc3f9037c61c871cb093c4cbc03451157641781
Dasturchi uz <sizning-emailingiz@gmail.com>
2025-08-24 19:22:16 +0500

    O'zgartirishlar kiritildi

--- RussianEnglishUzbekdictionary/app/src/main/java/com/example/russian_english_uzbek_dictionary/SplashActivity.java
+++ RussianEnglishUzbekdictionary/app/src/main/java/com/example/russian_english_uzbek_dictionary/SplashActivity.java
@@ -3,6 +3,8 @@ package com.example.russian_english_uzbek_dictionary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,7 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        TextView splashText = findViewById(R.id.splashText);
 
+        splashText.setText("Russian-Uzbek-English-dictionary");
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
